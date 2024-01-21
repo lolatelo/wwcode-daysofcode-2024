8b4a7e7a70de5b73d91c4cbbe95d94830d7932bd
Lola <46532198+lolatelo@users.noreply.github.com>
2024-01-20 21:34:42 -0500

    Update day6.py

--- python-challenge/day6.py
+++ python-challenge/day6.py
@@ -14,7 +14,11 @@ def count_char(str, char):
     i = 0
     cnt = 0
 
-  # optional - not necessary to string because for loop will already index
+# while loop is not necessary to string because for loop will already index
+# In Python, strings are inherently iterable, and you can loop over them directly in a for loop
+# without needing to convert them into a list of characters first.
+# Each iteration of the for loop will automatically give you one character from the string.
+
     while i < len(str):
         chars.append(str[i])
         i += 1
