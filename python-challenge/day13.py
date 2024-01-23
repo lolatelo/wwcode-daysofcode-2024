54cf95c3f18e8510dce7733471f00f2763b090d1
Lola <46532198+lolatelo@users.noreply.github.com>
2024-01-23 11:36:31 -0500

    Update day13.py

--- python-challenge/day13.py
+++ python-challenge/day13.py
@@ -2,19 +2,11 @@
 Day 13: Write a program to shuffle the elements of a list randomly.
 '''
 
-from random import choice
+from random import shuffle
 
-def shuffle(lst):
-    
-    shuffled_lst = []
-    
-    while len(shuffled_lst) < len(lst):
-        shuff_char = choice(lst)
-        if shuff_char not in shuffled_lst:
-            shuffled_lst.append(shuff_char)
-        
-    return shuffled_lst
-
+def shuffle_list(lst):
+    shuffle(lst)
+    return lst
 
 lst = [1, 'cat', 9, -1]
-print(shuffle(lst))
+print(shuffle_list(lst))
