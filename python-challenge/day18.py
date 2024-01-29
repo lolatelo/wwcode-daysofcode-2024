7c42018ece194d94e5488f109af5d519896be956
Lola <46532198+lolatelo@users.noreply.github.com>
2024-01-29 11:23:54 -0500

    Update day18.py

--- python-challenge/day18.py
+++ python-challenge/day18.py
@@ -10,3 +10,12 @@ def largest_three(num_1, num_2, num_3):
     20
     '''
     return max(num_1, num_2, num_3)
+
+def largest_number(*nums):
+    '''
+    >>> largest_number(1, 2, 3)
+    3
+    >>> largest_number(8, 20, 1)
+    20
+    '''
+    return max(nums)
