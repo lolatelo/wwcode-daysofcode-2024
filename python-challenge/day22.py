4fda699c71424ff059ed07f7900ad0db5f2d3908
Lola <46532198+lolatelo@users.noreply.github.com>
2024-02-03 11:41:00 -0500

    Create day22.py

---
+++ python-challenge/day22.py
@@ -0,0 +1,30 @@
+'''
+Day 22: Create a program to find the second-largest element in a list.
+'''
+
+def second_elem(lst):
+    '''
+    >>> second_elem([8, 9, 6, 0, 6])
+    6
+    >>> second_elem([1])
+    1
+    >>> second_elem([2, 1, 3])
+    1
+    '''
+
+    if len(lst) == 1:
+        return lst[0]
+
+    else:
+        first = 0
+        second = 0
+
+        for elem in lst:
+            if elem > first:
+                first = elem
+            elif elem > second:
+                second = elem
+            else:
+                pass
+    
+        return min(first, second)
