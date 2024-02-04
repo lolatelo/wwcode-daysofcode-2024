d3182c0c4824079b875e26b4ad2780aba77527de
Lola <46532198+lolatelo@users.noreply.github.com>
2024-02-04 16:00:19 -0500

    Create day25.py

---
+++ python-challenge/day25.py
@@ -0,0 +1,11 @@
+'''
+Day 25: Create a program to concatenate two lists.
+'''
+
+def concat_lsts(lst, lst2):
+    """
+    >>> concat_lsts(['cat', 'dog'], ['pineapple', 'apple', 'pear'])
+    ['cat', 'dog', 'pineapple', 'apple', 'pear']
+    """
+
+    return lst + lst2
