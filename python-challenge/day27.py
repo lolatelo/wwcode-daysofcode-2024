b9d0cf07d020a5e386da57f67146e08fd43240fc
Lola <46532198+lolatelo@users.noreply.github.com>
2024-02-05 22:30:31 -0500

    Create day27.py

---
+++ python-challenge/day27.py
@@ -0,0 +1,10 @@
+'''
+Day 27: Create a program that sorts a list of strings alphabetically.
+'''
+
+def sort_alpha(lst):
+    """
+    >>> sort_alpha(['cat', 'dog', 'person', 'apple', 'pear'])
+    ['apple', 'cat', 'dog', 'pear', 'person']
+    """
+    return sorted(lst)
