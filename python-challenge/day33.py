2672a6e1dcd238cfd31d0fb8b04a70d0b81620d9
Lola <46532198+lolatelo@users.noreply.github.com>
2024-02-25 21:07:52 -0500

    Create day33.py

---
+++ python-challenge/day33.py
@@ -0,0 +1,29 @@
+'''
+Day 33: Write a test case for a function that checks if a number is prime.
+'''
+from math import sqrt
+
+def prime(num):
+    '''
+    >>> prime(27)
+    False
+    >>> prime(11)
+    True
+    '''
+    if num == 1 or num == 0:
+        return False
+    
+    mults = range(2, int(sqrt(num))+1)
+
+    divisors = []
+
+    for i in mults:
+        if num % i == 0 and num != i:
+            divisors.append(False)
+        else:
+            divisors.append(True)
+    
+    if False in divisors:
+        return False
+    else:
+        return True
