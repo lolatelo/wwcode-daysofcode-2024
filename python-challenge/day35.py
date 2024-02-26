f360ffbc810656caa0f4302ffe4d1de38962cc6e
Lola <46532198+lolatelo@users.noreply.github.com>
2024-02-25 22:51:15 -0500

    Create day35.py

---
+++ python-challenge/day35.py
@@ -0,0 +1,10 @@
+'''
+Day 35: Write a simple unit test for a function that adds two numbers.
+'''
+
+def add_nums(num1, num2):
+    '''
+    >>> add_nums(1, 3)
+    4
+    '''
+    return num1 + num2
